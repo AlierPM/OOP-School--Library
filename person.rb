9ee31caabab540991b73269ad0d3a881cde4d659
Alier  Philip Maguet <alierditmaguet@gmail.com>
2022-11-19 13:25:44 -0800

    Create the many-to-many relationship

--- person.rb
+++ person.rb
@@ -1,16 +1,28 @@
-class Person
+require_relative './nameable'
+require_relative './rental'
+class Person < nameable
   attr_reader :id
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..10_000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
+    super()
   end
 
   def can_use_services?
     @parent_permission || is_of_age?
+  end
+
+  def correct_name
+    @name
+  end
+
+  def add_book_rental(book, date)
+    Rental.new(date, book, self)
   end
 
   private :of_age?
