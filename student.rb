fca89cdf30c23fcdd47ddbe71d5652978587b237
Alier  Philip Maguet <alierditmaguet@gmail.com>
2022-11-19 13:57:16 -0800

    Fix  rubocop linter errors

--- student.rb
+++ student.rb
@@ -2,6 +2,7 @@ require './person'
 
 class Student < Person
   attr_reader :classroom
+
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
